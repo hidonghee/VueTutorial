dc501e4ec8a8a3878b8f17bd77703b23bc8119ea
hidonghee <tlsehdgml159@gmail.com>
2023-05-04 11:20:08 +0900

    코지코더-mini project-ToDo List

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class ="container">
+    <h1 class ="text-center">Todo App</h1>
+    <input v-model="todoText" type="text" class="w-100 p-2" placeholder="Type todo" @keyup.enter="addTodo">
+    <hr>
+    <TodoItem v-for="todo in todos" :key="todo.id"
+    :todo="todo"
+    @toggle-checkbox="toggleCheckbox"
+    @click-delete = "deleteTodo"></TodoItem>  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoItem from '@/components/TodoItem.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoItem
+  },
+  data(){
+    return {
+      todoText: '',
+      todos: [
+        {
+          id:1,
+          text: 'buy a car',
+          checked : false
+        },
+        {
+          id:2,
+          text: 'play game',
+          checked : false
+        }
+      ]
+    }
+  },
+  methods:{
+    addTodo(e){
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = ''
+    },
+    toggleCheckbox({id, checked}){
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id){
+      // const index = this.todos.findIndex(todo => {
+      //   return todo.id ===id;
+      // });
+      // this.todos.splice(index,1)
+      console.log(id)
+      this.todos = this.todos.filter(todo => todo.id !== id);
+
+      const index = this.todos.findIndex(todo => {
+        return todo.id ===id;
+      });
+      this.todos.splice(index,1)
+      // console.log(id)
+      // this.todos = this.todos.filter(todo => todo.id !== id);s
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
